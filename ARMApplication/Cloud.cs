c27290d3f51032fed1037937ff2faf47e959ac63
shnatara <shnatara@SHRIRAM-DESK2>
2017-02-20 21:54:34 -0800

    Added Comments

--- ARMApplication/Cloud.cs
+++ ARMApplication/Cloud.cs
@@ -8,17 +8,72 @@ namespace ARMApplication
 {
     class Cloud
     {
+        /// <summary>
+        /// The token
+        /// </summary>
         private string _token;
+        /// <summary>
+        /// Gets or sets the client identifier.
+        /// </summary>
+        /// <value>
+        /// The client identifier.
+        /// </value>
         public string clientId { get; set; }
+        /// <summary>
+        /// Gets or sets the client secret.
+        /// </summary>
+        /// <value>
+        /// The client secret.
+        /// </value>
         public string clientSecret { get; set; }
+        /// <summary>
+        /// Gets or sets the name of the directory tenant.
+        /// </summary>
+        /// <value>
+        /// The name of the directory tenant.
+        /// </value>
         public string directoryTenantName { get; set; }
+        /// <summary>
+        /// Gets or sets the login endpoint.
+        /// </summary>
+        /// <value>
+        /// The login endpoint.
+        /// </value>
         public string loginEndpoint { get; set; }
+        /// <summary>
+        /// Gets or sets the arm resource identifier.
+        /// </summary>
+        /// <value>
+        /// The arm resource identifier.
+        /// </value>
         public string armResourceId { get; set; }
+        /// <summary>
+        /// Gets or sets the arm endpoint.
+        /// </summary>
+        /// <value>
+        /// The arm endpoint.
+        /// </value>
         public string armEndpoint { get; set; }
+        /// <summary>
+        /// Gets or sets the arm API version.
+        /// </summary>
+        /// <value>
+        /// The arm API version.
+        /// </value>
         public string armApiVersion { get; set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Cloud"/> class.
+        /// </summary>
         public Cloud() {}
 
+        /// <summary>
+        /// Uses ADAL to authenticate the Service Principal and returns the Authentication Result.
+        /// </summary>
+        /// <returns>
+        /// Authentication Result containing the Access Token
+        /// </returns>
+        /// <exception cref="InvalidOperationException">Could not get the token</exception>
         private async Task<AuthenticationResult> AuthenticateAsync()
         {
             var cc = new ClientCredential(this.clientId, this.clientSecret);
@@ -31,16 +86,27 @@ namespace ARMApplication
             return authenticationResult;
         }
 
+        /// <summary>
+        /// Authenticates the Service Principal and sets the token variable.
+        /// </summary>
         public void Authenticate()
         {
             Task<AuthenticationResult> test = AuthenticateAsync();
             _token = test.Result.AccessToken;
         }
 
+        /// <summary>
+        /// Gets the token.
+        /// </summary>
+        /// <returns></returns>
         public string GetToken() {
             return _token;
         }
 
+        /// <summary>
+        /// Lists all the subscriptions.
+        /// </summary>
+        /// <returns></returns>
         public string ListSubscriptions()
         {
             string url = String.Format("{0}subscriptions?api-version={1}",
@@ -49,6 +115,11 @@ namespace ARMApplication
             return CallAPI(url);
         }
 
+        /// <summary>
+        /// Gets the subscription by the subscription identifier.
+        /// </summary>
+        /// <param name="subscriptionId">The subscription identifier.</param>
+        /// <returns></returns>
         public string GetSubscriptionById(string subscriptionId)
         {
             string url = String.Format("{0}subscriptions/{1}?api-version={2}",
@@ -57,6 +128,11 @@ namespace ARMApplication
             return CallAPI(url);
         }
 
+        /// <summary>
+        /// Lists all the resource groups.
+        /// </summary>
+        /// <param name="subscriptionId">The subscription identifier.</param>
+        /// <returns></returns>
         public string ListResourceGroups(string subscriptionId)
         {
             string url = String.Format("{0}subscriptions/{1}/resourcegroups?api-version={2}",
@@ -65,6 +141,12 @@ namespace ARMApplication
             return CallAPI(url);
         }
 
+        /// <summary>
+        /// Gets the name of the resource group by its Name.
+        /// </summary>
+        /// <param name="subscriptionId">The subscription identifier.</param>
+        /// <param name="resourceGroupName">Name of the resource group.</param>
+        /// <returns></returns>
         public string GetResourceGroupByName(string subscriptionId, string resourceGroupName)
         {
             string url = String.Format("{0}subscriptions/{1}/resourcegroups/{2}?api-version={3}",
@@ -73,6 +155,12 @@ namespace ARMApplication
             return CallAPI(url);
         }
 
+        /// <summary>
+        /// Lists the resources in resource group.
+        /// </summary>
+        /// <param name="subscriptionId">The subscription identifier.</param>
+        /// <param name="resourceGroupName">Name of the resource group.</param>
+        /// <returns></returns>
         public string ListResourcesInResourceGroup(string subscriptionId, string resourceGroupName)
         {
             string url = String.Format("{0}subscriptions/{1}/resourcegroups/{2}/resources?api-version={3}",
@@ -81,6 +169,15 @@ namespace ARMApplication
             return CallAPI(url);
         }
 
+        /// <summary>
+        /// Lists all the resources in a namespace within a resource group.
+        /// </summary>
+        /// <param name="subscriptionId">The subscription identifier.</param>
+        /// <param name="resourceGroupName">Name of the resource group.</param>
+        /// <param name="resourceNamespace">The resource namespace.</param>
+        /// <param name="resourceTypeName">Name of the resource type.</param>
+        /// <param name="apiVersion">The API version.</param>
+        /// <returns></returns>
         public string ListResourcesByNamespaceInResourceGroup(string subscriptionId, string resourceGroupName, string resourceNamespace, string resourceTypeName, string apiVersion)
         {
             string url = String.Format("{0}subscriptions/{1}/resourcegroups/{2}/providers/{3}/{4}?api-version={5}",
@@ -89,6 +186,16 @@ namespace ARMApplication
             return CallAPI(url);
         }
 
+        /// <summary>
+        /// Lists the name of the resource by Name.
+        /// </summary>
+        /// <param name="subscriptionId">The subscription identifier.</param>
+        /// <param name="resourceGroupName">Name of the resource group.</param>
+        /// <param name="resourceNamespace">The resource namespace.</param>
+        /// <param name="resourceTypeName">Name of the resource type.</param>
+        /// <param name="resourceName">Name of the resource.</param>
+        /// <param name="apiVersion">The API version.</param>
+        /// <returns></returns>
         public string ListResourceByName(string subscriptionId, string resourceGroupName, string resourceNamespace, string resourceTypeName, string resourceName, string apiVersion)
         {
             string url = String.Format("{0}subscriptions/{1}/resourcegroups/{2}/providers/{3}/{4}/{5}?api-version={6}",
@@ -97,6 +204,12 @@ namespace ARMApplication
             return CallAPI(url);
         }
 
+        /// <summary>
+        /// Calls the API.
+        /// </summary>
+        /// <param name="url">The URL.</param>
+        /// <param name="urlParameters">The URL parameters.</param>
+        /// <returns></returns>
         public string CallAPI(string url, string urlParameters = "")
         {
             HttpClient client = new HttpClient();
