943ad642142bcd48e2bd685f5b180e01d33e0947
shnatara <shnatara@SHRIRAM-DESK2>
2017-02-20 18:10:29 -0800

    First Application Checkin

---
+++ ARMApplication/Cloud.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ARMApplication
+{
+    class Cloud
+    {
+        private string _token;
+        public string clientId { get; set; }
+        public string clientSecret { get; set; }
+        public string directoryTenantName { get; set; }
+        public string loginEndpoint { get; set; }
+        public string armResourceId { get; set; }
+        public string armEndpoint { get; set; }
+        public string armApiVersion { get; set; }
+
+        public Cloud() {}
+
+        private async Task<AuthenticationResult> AuthenticateAsync()
+        {
+            var cc = new ClientCredential(this.clientId, this.clientSecret);
+            var context = new AuthenticationContext(String.Concat(this.loginEndpoint, this.directoryTenantName));
+            var authenticationResult = await context.AcquireTokenAsync(this.armResourceId, cc);
+            if (authenticationResult == null)
+            {
+                throw new InvalidOperationException("Could not get the token");
+            }
+            return authenticationResult;
+        }
+
+        public void Authenticate()
+        {
+            Task<AuthenticationResult> test = AuthenticateAsync();
+            _token = test.Result.AccessToken;
+        }
+
+        public string GetToken() {
+            return _token;
+        }
+
+        public string ListSubscriptions()
+        {
+            string url = String.Format("{0}subscriptions?api-version={1}",
+                armEndpoint, armApiVersion);
+
+            return CallAPI(url);
+        }
+
+        public string GetSubscriptionById(string subscriptionId)
+        {
+            string url = String.Format("{0}subscriptions/{1}?api-version={2}",
+                armEndpoint, subscriptionId, armApiVersion);
+  
+            return CallAPI(url);
+        }
+
+        public string ListResourceGroups(string subscriptionId)
+        {
+            string url = String.Format("{0}subscriptions/{1}/resourcegroups?api-version={2}",
+                armEndpoint,subscriptionId, armApiVersion);
+
+            return CallAPI(url);
+        }
+
+        public string GetResourceGroupByName(string subscriptionId, string resourceGroupName)
+        {
+            string url = String.Format("{0}subscriptions/{1}/resourcegroups/{2}?api-version={3}",
+                armEndpoint, subscriptionId, resourceGroupName, armApiVersion);
+
+            return CallAPI(url);
+        }
+
+        public string ListResourcesInResourceGroup(string subscriptionId, string resourceGroupName)
+        {
+            string url = String.Format("{0}subscriptions/{1}/resourcegroups/{2}/resources?api-version={3}",
+                armEndpoint, subscriptionId, resourceGroupName, armApiVersion);
+
+            return CallAPI(url);
+        }
+
+        public string ListResourcesByNamespaceInResourceGroup(string subscriptionId, string resourceGroupName, string resourceNamespace, string resourceTypeName, string apiVersion)
+        {
+            string url = String.Format("{0}subscriptions/{1}/resourcegroups/{2}/providers/{3}/{4}?api-version={5}",
+                armEndpoint, subscriptionId, resourceGroupName, resourceNamespace, resourceTypeName, apiVersion);
+
+            return CallAPI(url);
+        }
+
+        public string ListResourceByName(string subscriptionId, string resourceGroupName, string resourceNamespace, string resourceTypeName, string resourceName, string apiVersion)
+        {
+            string url = String.Format("{0}subscriptions/{1}/resourcegroups/{2}/providers/{3}/{4}/{5}?api-version={6}",
+                armEndpoint, subscriptionId, resourceGroupName, resourceNamespace, resourceTypeName, resourceName, apiVersion);
+
+            return CallAPI(url);
+        }
+
+        public string CallAPI(string url, string urlParameters = "")
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+            
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+            
+            // List data response.
+            HttpResponseMessage response = client.GetAsync(urlParameters).Result;  // Blocking call!
+            return (response.IsSuccessStatusCode ? response.Content.ReadAsStringAsync().Result : String.Concat(response.StatusCode, response.ReasonPhrase));    
+        }
+
+    }
+}
---
+++ ARMApplication/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace ARMApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cloud azureCloud = new Cloud
+            {
+                clientId = "",
+                clientSecret = "",
+                loginEndpoint = "https://login.windows.net/",
+                directoryTenantName = "microsoft.onmicrosoft.com",
+                armEndpoint = "https://management.azure.com/",
+                armResourceId = "https://management.azure.com/",
+                armApiVersion = "2016-09-01"
+            };
+
+            // Authenticate to the specific Cloud's Resource Manager.
+            azureCloud.Authenticate();
+
+            //Console.WriteLine(azureCloud.GetToken());
+            string subscriptionId = "";
+            string resourceGroupName = "";
+
+            #region Subscription Methods
+            Console.WriteLine("Listing all subscriptions.");
+            Console.WriteLine(azureCloud.ListSubscriptions());
+            Console.WriteLine("Getting Subscription by Subscription Id.");
+            Console.WriteLine(azureCloud.GetSubscriptionById(subscriptionId));
+            #endregion
+
+            #region Resource Group Methods
+            Console.WriteLine("Listing all Resource Groups in a Subscription.");
+            Console.WriteLine(azureCloud.ListResourceGroups(subscriptionId));
+            Console.WriteLine("Listing Resource Group by Resource Group Name.");
+            Console.WriteLine(azureCloud.GetResourceGroupByName(subscriptionId, resourceGroupName));
+            Console.WriteLine("Listing all Resources in a Resource Group.");
+            Console.WriteLine(azureCloud.ListResourcesInResourceGroup(subscriptionId, resourceGroupName));
+            #endregion
+
+            #region Compute
+            string computeApiVersion = "2017-03-30";
+            string computeNamespace = "Microsoft.Compute";
+            
+            // Virtual Machines
+            Console.WriteLine("Listing Virtual Machines in a Resource Group");
+            Console.WriteLine(azureCloud.ListResourcesByNamespaceInResourceGroup(subscriptionId, resourceGroupName, computeNamespace, "virtualmachines", computeApiVersion));
+            Console.WriteLine("Listing Virtual machine by Name");
+            Console.WriteLine(azureCloud.ListResourceByName(subscriptionId, resourceGroupName, computeNamespace, "virtualmachines", "shriqpab7-mn0", computeApiVersion));
+            #endregion
+
+            #region Network
+            string networkApiVersion = "2016-12-01";
+            string networkNamespace = "Microsoft.Network";
+
+            // Virtual Networks
+            Console.WriteLine("Listing Virtual Networks in a Resource Group");
+            Console.WriteLine(azureCloud.ListResourcesByNamespaceInResourceGroup(subscriptionId, resourceGroupName, networkNamespace, "virtualnetworks", networkApiVersion));
+            Console.WriteLine("Listing Virtual Network by Name");
+            Console.WriteLine(azureCloud.ListResourceByName(subscriptionId, resourceGroupName, networkNamespace, "virtualnetworks", "shriqpab7vnet", networkApiVersion));
+
+            // Public IP Addresses
+            Console.WriteLine("Listing Public IP Addresses in a Resource Group");
+            Console.WriteLine(azureCloud.ListResourcesByNamespaceInResourceGroup(subscriptionId, resourceGroupName, networkNamespace, "publicIPAddresses", networkApiVersion));
+            Console.WriteLine("Listing Public IP Address by Name");
+            Console.WriteLine(azureCloud.ListResourceByName(subscriptionId, resourceGroupName, networkNamespace, "publicIPAddresses", "shriqpab7-publicip", networkApiVersion));
+            #endregion
+        }
+    }
+}
